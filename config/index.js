599bb52484f5c08ac946512fa5cbd97570a13bb6
Kostadin Terziev <kostadin_terziev@abv.bg>
2019-09-20 12:51:42 +0300

    change title

--- config/index.js
+++ config/index.js
@@ -1,15 +1,15 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Kostadin Terziev | Blog', // Navigation and Site Title
-  siteTitleAlt: 'Kostadin Terziev - Blog', // Alternative Site title for SEO
+  siteTitle: 'Kostadin Terziev', // Navigation and Site Title
+  siteTitleAlt: 'Kostadin Terziev', // Alternative Site title for SEO
   siteTitleManifest: 'KostadinTerziev',
   siteUrl: 'https://kostadin.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteHeadline: 'Writing and publishing content', // Headline for schema.org JSONLD
   siteBanner: '/social/banner.png', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'Minimalistic blog', // Your site description
+  siteDescription: 'Minimal personal website and blog', // Your site description
   author: 'KostadinTerziev', // Author for schemaORGJSONLD
   siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
 
